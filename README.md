bf70e2864a0d7fe6c765d9283b34f62c69b787a3
Mehmet Düzgün <mehmet.duzgun@obss.com.tr>
2020-10-11 19:29:21 +0600

    react part is added

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
+BE PART
+
 http://localhost:8080/main ==> Code Frontation 2020
+
+FE PART
+
+# install node to pc
+https://nodejs.org/en/download/
+
+cd *******/fe_src
+npm install
+##### npm i ace-builds
+npm start (localhost:3000)
---
+++ fe_src/src/modules/Compiler.js
@@ -0,0 +1,133 @@
+import React from 'react'
+
+const Compiler = () => {
+    var lang="java"
+    let ace = require('ace-builds/src-noconflict/ace');
+    let mode_java = require('ace-builds/src-noconflict/mode-java');//TODO if mode_java is not required, error is ocurred in htmEditor.getSession().setMode("ace/mode/java");
+    let theme_monokai = require('ace-builds/src-noconflict/theme-monokai'); //TODO if theme-monokai is not required, error is ocurred in htmEditor.setTheme("ace/theme/monokai");
+    const optionChanged = () => {
+        lang = document.getElementById("langOption").value;
+        if(lang=="java"){
+            initJavaEditor()
+        }else if(lang=="python"){
+            initPythonEditor()
+        }
+    };
+
+    const sendCode = (requestData,lang) => {
+        var resultArea = document.getElementById("resultArea");
+
+        // send data (method:post)
+        var xhr = new XMLHttpRequest();
+        var url = "http://localhost:8080/main/evaluate";
+        xhr.open("POST", url, true);
+        xhr.setRequestHeader("Content-Type", "application/json");
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState === 4 && xhr.status === 200) {
+                //var json = JSON.parse(xhr.responseText);
+                var resultString = xhr.responseText
+                resultArea.innerHTML = resultString.replace(/\n/g, "<br />");
+            }
+        };
+        var data = JSON.stringify({ "code": requestData, "lang":lang });
+        xhr.send(data);
+    };
+
+    const executeCode = () => {
+        var htmEditor = ace.edit("htmEditor");
+        sendCode("" + htmEditor.getValue(),lang);
+    }
+
+    const initEditor = () => {
+        var javaBegin =
+            "public class Solution {\n\n" +
+            "   public String solution(){\n" +
+            "       // calculate factorial :) \n" +
+            '       return "HELLO WORLD!!";\n' +
+            "   }" +
+            "\n}\n";
+
+        ace.require("ace/ext/language_tools");
+
+        var htmEditor = ace.edit("htmEditor");
+        htmEditor.getSession().setMode("ace/mode/java");
+        htmEditor.setTheme("ace/theme/monokai");
+        htmEditor.setOptions({
+            enableBasicAutocompletion: true,
+            enableSnippets: true
+        });
+        htmEditor.setFontSize(23);
+
+        //htmEditor.session.insert(0, javaBegin);//htmEditor.getCursorPosition()
+        htmEditor.setValue(javaBegin);
+        htmEditor.setShowPrintMargin(false);
+        htmEditor.setHighlightActiveLine(false);
+    }
+
+    const initJavaEditor = () => {
+        var javaBegin =
+            "public class Solution {\n\n" +
+            "   public String solution(){\n" +
+            "       // calculate factorial :) \n" +
+            '       return "HELLO WORLD!!";\n' +
+            "   }" +
+            "\n}\n";
+        ace.require("ace/ext/language_tools");
+
+        var htmEditor = ace.edit("htmEditor");
+        htmEditor.getSession().setMode("ace/mode/java");
+
+        htmEditor.setValue(javaBegin);
+
+    }
+
+
+    const initPythonEditor = () => {
+        var pythonBegin =
+            "class Solution :\n\n" +
+            "   def __init__(self,value):\n" +
+            '       self.value = value \n' +
+            "   def solution(self):\n" +
+            "       # calculate factorial :) \n" +
+            '       return "HELLO WORLD!!"\n' +
+            "\n}\n";
+        ace.require("ace/ext/language_tools");
+
+        var htmEditor = ace.edit("htmEditor");
+        htmEditor.getSession().setMode("ace/mode/python");
+
+        htmEditor.setValue(pythonBegin);
+
+    }
+
+    window.addEventListener('load', initEditor);
+
+
+  return (
+      <div className="container">
+          <h1>We Are The Champions!</h1>
+
+          <select id="langOption" className="option" onChange={() => optionChanged()}>
+              <option value="java">Java</option>
+              <option value="python">Python</option>
+          </select>
+
+          <div className="row">
+              <div className="col-sm-12" id="htmPane">
+                  <div className="inner" id="htmEditor"></div>
+              </div>
+
+          </div>
+          <button id="executeButton" type="button" className="btn btn-dark" onClick={() => executeCode()}>Execute</button>
+
+          <br></br>
+          <h3>Result:</h3>
+          <div id="resultArea" style={{background: 'honeydew'}}>
+
+          </div>
+
+      </div>
+  )
+}
+
+export default Compiler
