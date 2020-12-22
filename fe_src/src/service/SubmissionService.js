c93d006e19c96f7f5bed17f6592f7ebf386f66e1
AlperKocaman <alperkman@gmail.com>
2020-12-22 23:40:51 +0300

    Sonar results are shown in table.

--- fe_src/src/service/SubmissionService.js
+++ fe_src/src/service/SubmissionService.js
@@ -2,6 +2,16 @@ import axios from 'axios';
 
 export default class SubmissionService{
 
+    BACKEND_BASE_URL = "http://localhost:9000/api/";
+    FIRST_URL_PARAMETER = "measures/component?metricKeys=";
+    COMPLEXITY_METRICS = "complexity,cognitive_complexity";
+    DUPLICATION_METRICS = "duplicated_blocks,duplicated_files,duplicated_lines,duplicated_lines_density";
+    MAINTAINABILITY_METRICS = "code_smells,sqale_index";
+    RELIABILITY_METRICS = "bugs,reliability_rating,reliability_remediation_effort";
+    SECURITY_METRICS = "vulnerabilities,security_rating,security_remediation_effort,security_hotspots,security_review_rating";
+    SIZE_METRICS = "comment_lines,comment_lines_density,ncloc,functions";
+    LAST_URL_PARAMETER = "&component=";
+
     async requestToServer(url = '',method='', data = {}) {
         try {
             if (method=='POST'){
@@ -56,4 +66,35 @@ export default class SubmissionService{
         const response = await this.requestToServer(url, method, submission);
         return response;
     }
+
+    extractSonarKeyFromUrl(sonarUrl){
+        return sonarUrl.substring(35);
+    }
+
+    async getSonarMetrics(submission){
+        let response = [];
+        const complexityMetricsUrl=this.BACKEND_BASE_URL + this.FIRST_URL_PARAMETER + this.COMPLEXITY_METRICS + this.LAST_URL_PARAMETER +
+            this.extractSonarKeyFromUrl(submission.sonarUrl);
+        const duplicationMetricsUrl=this.BACKEND_BASE_URL + this.FIRST_URL_PARAMETER + this.DUPLICATION_METRICS + this.LAST_URL_PARAMETER +
+            this.extractSonarKeyFromUrl(submission.sonarUrl);
+        const maintainabilityMetricsUrl=this.BACKEND_BASE_URL + this.FIRST_URL_PARAMETER + this.MAINTAINABILITY_METRICS + this.LAST_URL_PARAMETER +
+            this.extractSonarKeyFromUrl(submission.sonarUrl);
+        const reliabilityMetricsUrl=this.BACKEND_BASE_URL + this.FIRST_URL_PARAMETER + this.RELIABILITY_METRICS + this.LAST_URL_PARAMETER +
+            this.extractSonarKeyFromUrl(submission.sonarUrl);
+        const securityMetricsUrl=this.BACKEND_BASE_URL + this.FIRST_URL_PARAMETER + this.SECURITY_METRICS + this.LAST_URL_PARAMETER +
+            this.extractSonarKeyFromUrl(submission.sonarUrl);
+        const sizeMetricsUrl=this.BACKEND_BASE_URL + this.FIRST_URL_PARAMETER + this.SIZE_METRICS + this.LAST_URL_PARAMETER +
+            this.extractSonarKeyFromUrl(submission.sonarUrl);
+        const method='GET';
+
+        response.push(await this.requestToServer(complexityMetricsUrl, method, submission));
+        response.push(await this.requestToServer(duplicationMetricsUrl, method, submission));
+        response.push(await this.requestToServer(maintainabilityMetricsUrl, method, submission));
+        response.push(await this.requestToServer(reliabilityMetricsUrl, method, submission));
+        response.push(await this.requestToServer(securityMetricsUrl, method, submission));
+        response.push(await this.requestToServer(sizeMetricsUrl, method, submission));
+        return response;
+    }
+
+
 }
