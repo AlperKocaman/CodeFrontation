cc839fb3a2007ed3ab5c3ba7dab192b77059d2b9
Ali Kaan <alikaanuyar@hotmail.com>
2020-12-24 02:51:24 +0300

    templates sayfasina problemler eklendi

--- src/main/java/tr/com/obss/codefrontation/mapper/TemplateMapper.java
+++ src/main/java/tr/com/obss/codefrontation/mapper/TemplateMapper.java
@@ -7,8 +7,10 @@ import org.mapstruct.MappingTarget;
 
 import tr.com.obss.codefrontation.dto.RoleDTO;
 import tr.com.obss.codefrontation.dto.TemplateDTO;
+import tr.com.obss.codefrontation.dto.UserDTO;
 import tr.com.obss.codefrontation.entity.Role;
 import tr.com.obss.codefrontation.entity.Template;
+import tr.com.obss.codefrontation.entity.User;
 
 @org.mapstruct.Mapper(componentModel = "spring")
 public interface TemplateMapper {
@@ -20,6 +22,7 @@ public interface TemplateMapper {
 	Template toTemplateEntity(TemplateDTO dto);
 	Role toRoleEntity(RoleDTO source);
 	RoleDTO toRoleDTO(Role source);
+	UserDTO toUserDTO(User source);
 	@Mapping(source = "authorId", target = "author.id")
 	@Mapping(source = "authorName", target = "author.username")
 	void updateEntity(TemplateDTO dto, @MappingTarget Template entity);
