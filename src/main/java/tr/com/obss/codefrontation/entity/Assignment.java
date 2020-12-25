e86ba2ef9b8e3d678731d3aef4c56a919e005d49
AlperKocaman <alperkman@gmail.com>
2020-12-25 14:12:32 +0300

    Annotatiton @Data put

--- src/main/java/tr/com/obss/codefrontation/entity/Assignment.java
+++ src/main/java/tr/com/obss/codefrontation/entity/Assignment.java
@@ -1,13 +1,11 @@
 package tr.com.obss.codefrontation.entity;
 
-import lombok.Getter;
-import lombok.Setter;
+import lombok.Data;
 
 import javax.persistence.*;
 import java.util.UUID;
 
-@Getter
-@Setter
+@Data
 @Entity
 @Table(name = "assignments")
 public class Assignment {
